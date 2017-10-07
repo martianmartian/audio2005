4bd2858bd57213ad108a35cb2ea730df0372c972
martian2049 <ginx.qx@gmail.com>
2017-10-07 22:51:09 +0800

    Initial Commit

---
+++ audio2005/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  audio2005
+//
+//  Created by martian2049 on 10/7/17.
+//  Copyright © 2017 martian2049. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
