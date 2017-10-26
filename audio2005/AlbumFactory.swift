dcf9e376b04d607ce9feb8fb6a6f2141740c2db7
martian2049 <ginx.qx@gmail.com>
2017-10-26 18:23:31 +0800

    oh no

--- audio2005/AlbumFactory.swift
+++ audio2005/AlbumFactory.swift
@@ -7,10 +7,18 @@ class AlbumFactory{
     static var viewAlbumId = String()
 
     static func getMediaAlbums() -> Array<MPMediaItemCollection>{
-        loghere()
         let query = MPMediaQuery.playlists()
         guard let albums = query.collections else {return Array<MPMediaItemCollection>()}
-        return albums
+        
+        var result = Array<MPMediaItemCollection>()
+        for album in albums{
+            let name = album.value(forProperty: MPMediaPlaylistPropertyName) as! String
+            if name != "Purchased"{
+                result.append(album)
+            }
+        }
+        
+        return result
     }
     
     
--- audio2005/IViewController.swift
+++ audio2005/IViewController.swift
@@ -3,50 +3,86 @@
 import UIKit
 import MediaPlayer
 
-class IViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate{
+class IViewController: UIViewController{
 
     var Albums = AlbumFactory.getMediaAlbums()
+    var collectionView : UICollectionView!
     
-    @IBOutlet weak var collectionView: UICollectionView!
-
-    override func loadView() {
-        super.loadView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         logvar("Albums", Albums)
-        
-//        let query = MPMediaQuery.playlists()
-//        guard let albums = query.collections else {return}
-//        let album = albums[0]
-//        let items = album.items
-//        print(type(of: items))
-//
-        
+        setupCollectionView()
     }
     
-    //Mark: View Display and interaction
-    func numberOfSections(in collectionView: UICollectionView) -> Int { return 1 }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int)->Int {return Albums.count}
+}
+extension IViewController: UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+    override func loadView() { UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation");super.loadView()}
+    override var shouldAutorotate: Bool { return false }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { return .portrait }
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation { return .portrait }
+    
+    func setupCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 15
+        layout.minimumInteritemSpacing = 5
+        
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        collectionView.register(albumCell.self, forCellWithReuseIdentifier: "albumCell")
+        collectionView.backgroundColor = UIColor.white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.alwaysBounceVertical = false
+        view.addSubview(collectionView)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,layout collectionViewLayout:UICollectionViewLayout,insetForSectionAt section: Int)->UIEdgeInsets { return UIEdgeInsetsMake(18, 0, 0, 0) }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {return 1}
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int)->Int{return Albums.count}
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let album = Albums[indexPath.row]
         let name = album.value(forProperty: MPMediaPlaylistPropertyName) as! String
         
-        let albumCell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! albumCell
-        albumCell.albumImage.image = UIImage(named: albumCovers[indexPath.row])
-        albumCell.imageLabel.text = name
+        let albumCell  = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! albumCell
+        albumCell.awakeFromNib()
+        albumCell.im.image = UIImage(named: albumCovers[indexPath.row])
+        albumCell.codedLabel.text = name
         albumCell.isUserInteractionEnabled=true
-
+        
         return albumCell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath:IndexPath)->CGSize {
+        let w = (collectionView.frame.size.width-20) / 3
+        return CGSize(width: CGFloat(w), height: CGFloat(w+20))
+//        return CGSize(width: view.frame.width/4, height: view.frame.width/4 + 20)
+    }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        IIVCData.viewAlbumIndex = indexPath.row
         logmark("Going to album: \(indexPath.row)")
+        IIVCData.viewAlbumIndex = indexPath.row
         self.performSegue(withIdentifier: "oneTotwo", sender: self)
     }
     
 }
-
 class albumCell: UICollectionViewCell {
+    var im : UIImageView!
+    var codedLabel : UILabel!
     
-    @IBOutlet weak var albumImage: UIImageView!
-    
-    @IBOutlet weak var imageLabel: UILabel!
+    override func awakeFromNib() {
+        im = UIImageView()
+        im.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        im.contentMode = .scaleAspectFit
+        im.clipsToBounds = true
+        
+        self.contentView.addSubview(im)
+        
+        codedLabel = UILabel()
+        codedLabel.contentMode = .bottom
+        codedLabel.frame = CGRect(x: 0, y: frame.width, width: frame.width, height: 20)
+        codedLabel.textAlignment = .center
+        codedLabel.numberOfLines=1
+        //codedLabel.textColor=UIColor.white
+        //codedLabel.backgroundColor=UIColor.blue
+        self.contentView.addSubview(codedLabel)
+        //Remember to add UI Element to the contentView, not the cell itself
+    }
 }
